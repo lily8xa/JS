e07ab21b32a0d8c43d211199e373b157dc19855a
lily8xa <lily8xa@gmail.com>
2026-02-08 17:53:33 +0200

    add home 3+2

---
+++ HOME3/1/2/Java3home+.js
@@ -0,0 +1,55 @@
+// #yHAwJOyiC
+//
+// – Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
+//
+let arrFirst=[125,2553,1,15455,12.21,0,4555,969696,6969,301]
+for (let i=0;i<arrFirst.length;i++){
+    console.log(arrFirst[i])
+}
+  console.log(  `#GamKju89ob`)
+//
+// – Створити масив з 10 строкових елементів. Вивести в консоль всі його елементи в циклі.
+let arrSecond=[`It`,`The`,`Best`,`Day`,`Of`,`My`,`Life`,`Oh`,`Fo`,`You`]
+for(let i=0; i<arrSecond.length;i++){
+    console.log(arrSecond[i])
+}
+//
+console.log(`#Bm76xmg`)
+//
+// – Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
+//
+let arrTse=[`great`,69,true,96,false,`Impotent`,true,25,false,`Ok`]
+let j=0;
+while(j<arrTse.length){
+    console.log(arrTse[j])
+    j++;
+}
+console.log( `#u3vmD0YJXh`)
+//
+// – Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки булеві елементи
+let arrFor=[`great`,69,true,96,false,`Impotent`,true,25,false,`Ok`]
+for(let i=0; i<arrFor.length;i++){
+    if(typeof arrFor[i]==="boolean"){
+        console.log(arrFor[i])
+    }
+}
+//
+console.log(` #9stMq2ou`)
+//
+// – Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки числові елементи
+let arrFive=[`great`,69,true,96,false,`Impotent`,true,25,false,`Ok`]
+for(let i=0; i<arrFive.length; i++){
+    if(typeof arrFive[i]==='number'){
+        console.log(arrFive[i])
+    }
+}
+//
+console.log(`// #mK4pmM4`)
+//
+// – Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки рядкові елементи
+let arrSix=[`great`,69,true,`The`,`Best`,`Day`,96,false,`Impotent`,`Yoy-yoy`];
+for(let i=0; i<arrSix.length;i++){
+    if(typeof arrSix[i]==="string"){
+        console.log(arrSix[i])
+    }
+}
---
+++ HOME3/1/Home1/Home3+1.js
@@ -0,0 +1,115 @@
+
+
+let ArrFirst=[351,12,-15236,3,100500]
+console.log(ArrFirst)
+
+let ArrSecond=[`I`,`Love`,`you`,`Baby`,`Sting`]
+console.log(ArrSecond)
+
+let ArrThird=[`I`,`Love`,`you`,100,true]
+console.log(ArrThird)
+
+let ArrFour=[`I`,`Love`,`You`,`love`,`me`,true]
+console.log(ArrFour[2])
+console.log(ArrFour[1])
+console.log(ArrFour[4])
+
+
+let arrFive=[2,17,13,6,22,31,45,66,100,-18]
+let i=0;
+while(i<arrFive.length){
+    console.log(arrFive[i])
+    i++;
+}
+for(let i=0; i<arrFive.length; i++){
+    console.log(arrFive[i]);
+}
+
+let j=1;
+while(j<arrFive.length){
+    {
+        console.log(arrFive[j]);
+    }
+    j+=2;
+}
+
+for(let i=1; i<arrFive.length; i+=2){
+    console.log(arrFive[i]);
+}
+console.log(`5parni`);
+
+let f=0
+while(f<arrFive.length) {
+    if (arrFive [f] %2===0)
+    {
+        console.log(arrFive[f]);
+    }
+    f++;
+}
+
+for(let g=0; g<arrFive.length; g++)
+if (arrFive[g]%2===0)
+{
+    console.log(arrFive[g]);
+}
+
+for(let g=0; g<arrFive.length;g++)
+    if (arrFive[g]%3===0)
+    {
+        arrFive[g]=`Okten`
+    }
+console.log(arrFive)
+
+arrFive.reverse();
+console.log(arrFive)
+
+
+while(i<arrFive.length-1){
+    console.log(arrFive[i])
+    i--;
+}
+console.log(`2reversFor`)
+for(let i= arrFive.length -1; i>=0;i--){
+    console.log(arrFive[i]);
+}
+
+
+
+ console.log(`3reversIN`)
+
+let arrSet=[2,17,13,6,22,31,45,66,100,-18]
+let o=arrSet.length-1;
+while(o>=0){
+    if(o %2!==0)
+    {
+        console.log(arrSet[o]);
+
+    }
+    o--;
+}
+
+for(let i=arrSet.length-1; i>=0; i--)
+{
+    if(i%2!==0)
+    console.log(arrSet[i]);
+}
+console.log(`4reversFor`)
+let y=arrSet.length-1;
+while(y>=0){
+    if(arrSet[y]%2===0){
+        console.log(arrSet[y]);
+    }
+    y--;
+}
+
+for(let i=arrSet.length-1; i>=0; i--){
+    if(arrSet[i]%2===0)
+        console.log(arrSet[i]);
+}
+
+for(let i=arrSet.length-1; i>=0; i--){
+    if(arrSet[i]%3===0){
+        arrSet[i]=`Okten`
+    }
+}
+console.log(arrSet);
