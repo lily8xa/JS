b6b7a3b128b32a8753fbc8fb665308b80262f2d8
lily8xa <lily8xa@gmail.com>
2026-03-02 21:44:33 +0200

    add home 4,1

--- Home4/HW4,1/java4,1.js
+++ Home4/HW4,1/java4,1.js
@@ -123,3 +123,38 @@ console.log(minValue([12,-22,88,-12,-36,]));
  console.log(sum([1,2,10]))
 
 
+
+//– створити функцію swap(arr,index1,index2). Функція міняє місцями значення у відповідних індексах
+//
+// Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
+
+function swap(arr,i1,i2){
+     let temp=arr[i1];
+     arr[i1]=arr[i2];
+     arr[i2]=temp;
+     return arr;
+}
+console.log(swap([11,22,33,44],1,0));
+
+
+
+ // Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
+//
+// Приклад exchange(10000,[{currency:’USD’,value:25},{currency:’EUR’,value:42}],’USD’) // => 400
+
+function exchange(sumUAH,currencyValues ,exchangeCurrency){
+
+for (let i=0; i<currencyValues.length; i++){
+    if(currencyValues[i].currency===exchangeCurrency){
+
+        return sumUAH/currencyValues[i].value
+
+    }
+    // else if(currencyValues[i].currency!==exchangeCurrency){   //Я хотіла у випадку неправильного вводу вивести повідомлення.
+    //    console.log(`Error`);
+    // }
+}
+
+}
+ // exchange(10000,[{currency:`’USD’`,value:25},{currency:`’EUR’`,value:42}],)
+console.log (exchange(10000,[{currency:`USD`,value:25},{currency:`EUR`,value:42}],`EUR`))
